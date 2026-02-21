dc0297d3dd6e2a7c6ce1d91c4868d880aa2aff6d
Henry Stov <henrystov7@outlook.com>
2026-02-20 21:32:13 -0500

    Update boost.js

--- boost.js
+++ boost.js
@@ -4,19 +4,23 @@ const client = new SteamUser();
 const logOnOptions = {
     accountName: process.env.STEAM_USER,
     password: process.env.STEAM_PASSWORD,
+    // This is for the 5-character code from your email
     authCode: process.env.STEAM_GUARD_CODE 
 };
-
-const games = process.env.GAMES.split(',').map(id => parseInt(id.trim()));
 
 client.logOn(logOnOptions);
 
 client.on('loggedOn', () => {
-    console.log("Logged into Steam");
-    client.setGamesPlayed(games);
+    console.log("SUCCESS: Logged into Steam via Email Guard!");
+    client.setGamesPlayed([440, 570, 730]);
 });
 
 client.on('error', (err) => {
-    console.log("Error: " + err.message);
-    process.exit(1);
+    if (err.eresult === SteamUser.EResult.AccountLogonDenied) {
+        console.log("!!! STEAM GUARD REQUIRED !!!");
+        console.log("Check your email for a 5-character code.");
+        console.log("Add it to GitHub Secrets as 'STEAM_GUARD_CODE' and run again.");
+    } else {
+        console.log("Login Error: " + err.message);
+    }
 });
