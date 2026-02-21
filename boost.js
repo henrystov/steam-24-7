9c21461c9c66c8d8e0d2a8c1c1e421505d4042a5
Henry Stov <henrystov7@outlook.com>
2026-02-21 13:19:22 -0500

    Update boost.js

--- boost.js
+++ boost.js
@@ -1,26 +1,36 @@
+// boost.js
 const SteamUser = require('steam-user');
 const client = new SteamUser();
 
 const logOnOptions = {
     accountName: process.env.STEAM_USER,
     password: process.env.STEAM_PASSWORD,
-    // This is for the 5-character code from your email
-    authCode: process.env.STEAM_GUARD_CODE 
+    authCode: process.env.STEAM_GUARD_CODE // The 5-char code from your email
 };
 
 client.logOn(logOnOptions);
 
-client.on('loggedOn', () => {
-    console.log("SUCCESS: Logged into Steam via Email Guard!");
+client.on('loggedOn', (details) => {
+    console.log("SUCCESS: Logged into Steam as " + client.steamID.getSteamID64());
+    
+    // Set games to "online" status to boost hours
+    // 440: Team Fortress 2, 570: Dota 2, 730: CS2
     client.setGamesPlayed([440, 570, 730]);
+    console.log("Now boosting hours for IDs: 440, 570, 730");
+});
+
+client.on('steamGuard', (domain, callback, lastCodeWrong) => {
+    if (lastCodeWrong) {
+        console.log("The Steam Guard code in GitHub Secrets is WRONG or EXPIRED.");
+        process.exit(1); 
+    } else {
+        console.log("Steam Guard code required from email: " + domain);
+        // If the code wasn't in logOnOptions, the script would usually wait here.
+        // In a CI environment, we want to fail fast so the user knows to update secrets.
+    }
 });
 
 client.on('error', (err) => {
-    if (err.eresult === SteamUser.EResult.AccountLogonDenied) {
-        console.log("!!! STEAM GUARD REQUIRED !!!");
-        console.log("Check your email for a 5-character code.");
-        console.log("Add it to GitHub Secrets as 'STEAM_GUARD_CODE' and run again.");
-    } else {
-        console.log("Login Error: " + err.message);
-    }
+    console.log("Login Error: " + err.message);
+    process.exit(1);
 });
